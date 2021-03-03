3b758b45e412f47e41c76dc6805bf7ee2718c359
Oswalt Mosby <0218710@up.edu.mx>
2021-03-03 04:17:24 -0600

    init

---
+++ Pacman/Assets/Scripts/Controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float MoveSpeed= 5000f;
+    Animator animator;
+    Rigidbody2D body;
+    SpriteRenderer flip;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        body= GetComponent<Rigidbody2D>();
+        flip = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+ 
+        /* if(! Input.GetKeyDown(KeyCode.W) && !Input.GetKeyDown(KeyCode.S )){
+            body.velocity = new Vector2(0,0);
+            animator.SetBool("KeyDown",false);
+            
+        } */
+        if(Input.GetKey(KeyCode.W)){
+            body.velocity = new Vector2(0,MoveSpeed * Time.deltaTime);
+            animator.SetBool("Direction",true);
+            flip.flipY = true;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            body.velocity = new Vector2(0,-MoveSpeed * Time.deltaTime);
+            animator.SetBool("Direction",true);
+            flip.flipY = false;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            body.velocity = new Vector2(-MoveSpeed * Time.deltaTime,0);
+            animator.SetBool("Direction",false);
+            flip.flipX = true;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            body.velocity = new Vector2(MoveSpeed * Time.deltaTime,0);
+            animator.SetBool("Direction",false);
+            flip.flipX = false;
+        }
+        
+    }
+}
---
+++ Pacman/Assets/Scripts/Fantasmas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour
+{
+    public SpriteRenderer hacha;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        hacha.enabled = false;
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        hacha.enabled = true;
+    }
+}
